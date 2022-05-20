943a3a27101932176c704e52ffe72cee81deaeb4
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-05-20 17:09:31 +0530

    auth service createUser created

--- auth-task-management/src/auth/auth.service.ts
+++ auth-task-management/src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthEntity } from './auth.entity';
+import { AuthType } from './auth.graphql';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectRepository(AuthEntity) private authRepository: Repository<AuthEntity>) {}
+
+    async createUser(username, password): Promise<AuthType> {
+      const user = this.authRepository.create({
+          username,
+          password
+      })
+
+      await this.authRepository.save(user);
+
+      return user
+    }
+}
