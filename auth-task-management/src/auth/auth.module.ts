4ba8b7b8c45bdf41f780e024108ffc5f217f23e8
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-05-31 10:01:04 +0530

    JWT and passport module setup done

--- auth-task-management/src/auth/auth.module.ts
+++ auth-task-management/src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthEntity } from './auth.entity';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuthEntity])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
+    TypeOrmModule.forFeature([AuthEntity])],
   providers: [AuthResolver, AuthService]
 })
 export class AuthModule {}
