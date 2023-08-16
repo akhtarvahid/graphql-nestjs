c92b40219afe9f3ae3a8eba51920b90e1539db78
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-08-16 15:19:34 +0530

    unused file removed

--- blogging/src/app.module.ts
+++ blogging/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -25,6 +24,6 @@ import { AuthModule } from './auth/auth.module';
     BlogModule,
     AuthModule,
   ],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
