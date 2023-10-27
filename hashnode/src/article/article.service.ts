d7bb739d2c49d373867459894994711af85b69fa
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-10-27 21:00:21 +0530

    users dislikes articles (dislike article)

--- hashnode/src/article/article.service.ts
+++ hashnode/src/article/article.service.ts
@@ -134,4 +134,25 @@ export class ArticleService {
 
       return article;
     }
+
+    async deleteArticleFromFavorites(slug: string, userId: number): Promise<ArticleEntity> {
+      const article = await this.findBySlug(slug);
+      const user = await this.userRepository.findOne({
+          where: { id: userId },
+          relations: ['favorites']
+      })
+
+      const articleIndex = user.favorites.findIndex(
+        (articleInFavorites) => articleInFavorites.id === article.id
+      );
+
+      if(articleIndex >=0 ) {
+        user.favorites.splice(articleIndex, 1);
+        article.favoritesCount--;
+        await this.userRepository.save(user);
+        await this.articleRepository.save(article);
+      }
+
+      return article;
+    }
 }
