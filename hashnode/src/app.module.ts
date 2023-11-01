d03ca2935e12ad66ff547c49bbdf186759c64190
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-11-01 12:12:24 +0530

    profile module created

--- hashnode/src/app.module.ts
+++ hashnode/src/app.module.ts
@@ -7,13 +7,15 @@ import { UserModule } from './user/user.module';
 import ormconfig from '@app/ormconfig';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
 import { ArticleModule } from './article/article.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
     TagModule,
     UserModule,
-    ArticleModule
+    ArticleModule,
+    ProfileModule
   ],
   controllers: [AppController],
   providers: [AppService],
