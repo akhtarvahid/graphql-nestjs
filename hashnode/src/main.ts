3e870fe8c591cbfb9d8a4f97eb109c715358e4ed
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-08-07 16:21:09 +0530

    swagger setup

--- hashnode/src/main.ts
+++ hashnode/src/main.ts
@@ -3,10 +3,14 @@ if(!process.env.IS_TS_NODE) {
 }
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(`App running on port: 3000`)
+  swaggerConfig(app);
+  console.log(`Swagger is live - http://localhost:3000/api#/`)
+
   await app.listen(3000);
 }
 bootstrap();
